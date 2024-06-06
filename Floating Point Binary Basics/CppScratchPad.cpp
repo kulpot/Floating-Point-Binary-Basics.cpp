636602b848cfafc19c5ec3bb1c193cee6cd4f823
Kulpot <kulpotwahu@gmail.com>
2024-06-07 01:09:00 +0800

    CppScratchPad.cpp-> #include <iostream> using std::cout; using std::endl;
    //Floating Point Binary Basics //ref link:https://www.youtube.com/watch?v=4eyc_9uZ-qk&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N
    
    void main()
    {
     // 654.321
    }

---
+++ Floating Point Binary Basics/CppScratchPad.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+
+//Floating Point Binary Basics //ref link:https://www.youtube.com/watch?v=4eyc_9uZ-qk&list=PLRwVmtr-pp04ljVWgUFlQoBl9tqTzFh2N
+
+void main()
+{
+	// 654.321
+}
